2a6a8fadba8aaa2381799e715e0f04501907c8cb
johan <johan52754@gmail.com>
2022-01-05 09:25:16 -0500

    Deploy netlify

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin =require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    mode:"production",
+    entry:"./src/index.js",
+    output:{
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.[contenthash].js"
+    },
+    resolve:{
+        extensions: ['.js','.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test : /\.(js|jsx)$/,
+                exclude : /node_modules/,
+                use: {
+                    loader:"babel-loader",
+                }
+            },
+            {
+                test : /\.html$/,
+                use:{
+                    loader:"html-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use:[MiniCssExtractPlugin.loader,"css-loader"]
+            },
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            inject:true,
+            template: "./public/index.html",
+            filename :"./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename:"assets/[name].[contenthash].css"
+        })
+    ],
+    optimization:{
+        minimize:true,
+        minimizer:[
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ]
+    }
+}
